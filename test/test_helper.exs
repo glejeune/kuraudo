600207d1b22309d536d967c9d999dfba5e23b1a1
glejeune <gregoire.lejeune@gmail.com>
2013-10-03 00:31:02 +0200

    Fix typo

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,21 +2,21 @@ ExUnit.start
 
 defmodule Kuraudo.Test.Configuration do
   def load_test_configuration() do
-  	conf_file = Path.join(__DIR__, "_test_conf.exs")
-  	unless File.exists?(conf_file) do
-  	  IO.puts "----------------------------------------------------------"
-  	  IO.puts "    _       No configuration file found."
+    conf_file = Path.join(__DIR__, "_test_conf.exs")
+    unless File.exists?(conf_file) do
+      IO.puts "----------------------------------------------------------"
+      IO.puts "    _       No configuration file found."
       IO.puts "   / \\"
       IO.puts "  / | \\    To run test, you need to create a file named"
       IO.puts " /  o  \\   `_test_conf.exs` in the `test` directory."
       IO.puts "/_______\\  Please, read the documentation if you need more"
       IO.puts "           informations"
-  	  IO.puts "----------------------------------------------------------"
-  	  exit(:normal)
-  	else
+      IO.puts "----------------------------------------------------------"
+      exit(:normal)
+    else
       {konfig , _} = Code.eval_string("konfig = " <> File.read!(conf_file))
       {:ok, konfig}
-  	end
+    end
   end
 
   defmacro unless_skipe(env, scope, options) do
